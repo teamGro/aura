d69fd016e0e496a05dddba917a18c20dd749cc37
Tima <bredos11@yandex.ru>
2020-12-01 09:51:05 +0300

    added brand

--- dist/script.js
+++ dist/script.js
@@ -325,10 +325,9 @@ let brandsData = [
   },
 ];
 
-let dataBrandDesc = [
+let brandDescData = [
   {
     title: 'many-furniture',
-    category: 'shop-cabinet',
     mainDesc: '<b>Мебельная компания №1 в России!</b><br> Собственные фабрика, складские помещения и транспорт, партнерские склады, быстрая доставка',
     logo: './assets/img/shop/brands/many-furniture.png',
     img: './assets/img/shop/brands/sofa.jpg',
@@ -337,6 +336,18 @@ let dataBrandDesc = [
     phone: '8 (927) 510-45-55',
     phoneLink: '+79275104555',
     url: 'https://mnogomebeli.com/',
+    link: '#',
+  },
+  {
+    title: 'ormatek',
+    mainDesc: '<b>Мебельная компания №1 в России!</b><br> Собственные фабрика, складские помещения и транспорт, партнерские склады, быстрая доставка',
+    logo: './assets/img/shop/brands/ormatek.png',
+    img: './assets/img/shop/brands/sofa.jpg',
+    alt: 'Ориатек',
+    desc: 'Орматек — сеть мебельных магазинов по всей России, специализарующаяся на продаже и изготовлении диванов, корпусной мебели и др.',
+    phone: '8 (927) 510-45-55',
+    phoneLink: '+79275104555',
+    url: 'https://ormatek.com/',
     link: '#',
   },
 ];
@@ -348,113 +359,7 @@ createFilters(filtersData, $('.filters'));
 createListWithBrands(brandsData, 'shop-soft', $('.brands'));
 let brandsList = $('.brands');
 brandsList.attr('data-category', 'shop-soft');
-
-function createDescForBrand(data, currentID, parent, nodeForRemoving) {
-  function createLayout(elem) {
-    return `
-        <button class="brand-desc__btn" type="button">
-            <span class="brand-desc__btn-arrow"></span>
-            <span class="brand-desc__btn-text">Назад</span>
-        </button>
-        <div class="brand-desc__wrap brand-desc__wrap_left">
-            <img class="brands-desc__img" src="${elem.img}" alt="${elem.alt}">
-        </div>
-
-        <div class="brand-desc__wrap brand-desc__wrap_right">
-             <div class="brand-desc__box">
-                <div class="brand-desc__logo-wrap">
-                    <img src="${elem.logo}" class="brand-desc__logo">
-                </div>
-
-                <p class="brand-desc__main">${elem.mainDesc}</p>
-                <p class="brand-desc__sub">${elem.desc}</p>
-             </div>
-
-             <ul class="brand-desc__list">
-                <li class="brand-desc__item">
-                    <a href="${elem.phoneLink}" class="brand-desc__link">${elem.phone}</a>
-                </li>
-                <li class="brand-desc__item">
-                    <a href="${elem.url}" class="brand-desc__link">${elem.url}</a>
-                </li>
-                <li class="brand-desc__item">
-                    <a href="${elem.link}" class="brand-desc__link brand-desc__link_download">Скачать каталог</a>
-                </li>
-            </ul>       
-        </div>
-        `;
-  }
-
-  for (let i = 0; i < data.length; i++) {
-    let item = data[i];
-    if (item.title == currentID) {
-      nodeForRemoving.empty();
-      parent.append(createLayout(item));
-      parent.attr('data-category', item.category);
-    }
-  }
-}
-
-function createListWithBrands(data, currentID, parent) {
-  function createLayout(elem) {
-    return `
-        <li class="brands__item" id="${elem.id}" data-type="${elem.category}">
-            <div class="brands__item-wrap">
-                <img src="${elem.img}"  alt="${elem.alt}" class="brands__img">
-            </div>
-        </li>
-        `;
-  }
-
-  for (let i = 0; i < data.length; i++) {
-    let item = data[i];
-    if (item.category == currentID) {
-      parent.append(createLayout(item));
-    }
-  }
-}
-
-function createSlider(data, parent) {
-  function createMarkup(elem) {
-    return `
-        <li class="${elem.classes}" id="${elem.id}">
-            <div class="shop__item-wrap">
-                <img src="${elem.img}"  alt="" class="shop__img">
-                <h3 class="shop__title">${elem.title}</h3>
-            </div>
-        </li>`;
-  }
-
-  data.forEach((item) => {
-    parent.append(createMarkup(item));
-  });
-
-  new Glide('.glide', {
-    type: 'carousel',
-    perView: 6,
-  }).mount();
-
-  let imgHeight = $('.shop__img').height();
-  let btnNext = $('.shop__btn-wrap').css('top', `${imgHeight / 2}px`);
-
-  //$('.shop').append(parent);
-}
-
-function createFilters(data, parent) {
-  function createFilterMarkup(elem) {
-    return `
-        <li class="${elem.classes}" id="${elem.id}">
-            <button class="shop-btn filters__btn">
-                <span class="filters__desc">${elem.title}</span>
-            </button>
-        </li>
-        `;
-  }
-
-  data.forEach((item) => {
-    parent.append(createFilterMarkup(item));
-  });
-}
+let activeCatergory = 'shop-soft';
 
 let filters = $('.filters');
 let currentActiveFilter = $('.filters__item_active');
@@ -478,9 +383,11 @@ shopList.on('click', (e) => {
     let targetID = target.attr('id');
 
     if (brandsList.attr('data-category') == targetID) return;
+
     brandsList.empty();
-    createListWithBrands(brandsData, targetID, $('.brands'));
+    createListWithBrands(brandsData, targetID, $('.brands'), targetID);
     brandsList.attr('data-category', targetID);
+    activeCatergory = targetID;
   }
 });
 
@@ -497,10 +404,118 @@ brandsList.on('click', function (e) {
   let target = $(e.target);
   target = target.closest('li');
   let targetID = target.attr('id');
-  createDescForBrand(dataBrandDesc, targetID, $('.brand-desc'), $(this));
+  createDescForBrand(brandDescData, targetID, $('.brand-desc'), $(this));
 
   $('.brand-desc__btn').on('click', () => {
-    createListWithBrands(brandsData, $('.brand-desc').attr('data-category'), $('.brands'));
+    console.log(activeCatergory);
+    createListWithBrands(brandsData, activeCatergory, $('.brands'));
     $('.brand-desc').empty();
   });
 });
+
+function createDescForBrand(data, currentID, parent, nodeForRemoving) {
+  function createLayout(elem) {
+    return `
+          <button class="brand-desc__btn" type="button">
+              <span class="brand-desc__btn-arrow"></span>
+              <span class="brand-desc__btn-text">Назад</span>
+          </button>
+          <div class="brand-desc__wrap brand-desc__wrap_left">
+              <img class="brands-desc__img" src="${elem.img}" alt="${elem.alt}">
+          </div>
+  
+          <div class="brand-desc__wrap brand-desc__wrap_right">
+               <div class="brand-desc__box">
+                  <div class="brand-desc__logo-wrap">
+                      <img src="${elem.logo}" class="brand-desc__logo">
+                  </div>
+  
+                  <p class="brand-desc__main">${elem.mainDesc}</p>
+                  <p class="brand-desc__sub">${elem.desc}</p>
+               </div>
+  
+               <ul class="brand-desc__list">
+                  <li class="brand-desc__item">
+                      <a href="${elem.phoneLink}" class="brand-desc__link">${elem.phone}</a>
+                  </li>
+                  <li class="brand-desc__item">
+                      <a href="${elem.url}" class="brand-desc__link">${elem.url}</a>
+                  </li>
+                  <li class="brand-desc__item">
+                      <a href="${elem.link}" class="brand-desc__link brand-desc__link_download">Скачать каталог</a>
+                  </li>
+              </ul>       
+          </div>
+          `;
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    let item = data[i];
+    if (item.title == currentID) {
+      nodeForRemoving.empty();
+      parent.append(createLayout(item));
+      parent.attr('data-category', currentID);
+    }
+  }
+}
+
+function createListWithBrands(data, currentID, parent) {
+  function createLayout(elem) {
+    return `
+          <li class="brands__item" id="${elem.id}" data-type="${elem.category}">
+              <div class="brands__item-wrap">
+                  <img src="${elem.img}"  alt="${elem.alt}" class="brands__img">
+              </div>
+          </li>
+          `;
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    let item = data[i];
+    if (item.category == currentID) {
+      parent.append(createLayout(item));
+    }
+  }
+}
+
+function createSlider(data, parent) {
+  function createMarkup(elem) {
+    return `
+          <li class="${elem.classes}" id="${elem.id}">
+              <div class="shop__item-wrap">
+                  <img src="${elem.img}"  alt="" class="shop__img">
+                  <h3 class="shop__title">${elem.title}</h3>
+              </div>
+          </li>`;
+  }
+
+  data.forEach((item) => {
+    parent.append(createMarkup(item));
+  });
+
+  new Glide('.glide', {
+    type: 'carousel',
+    perView: 6,
+  }).mount();
+
+  let imgHeight = $('.shop__img').height();
+  let btnNext = $('.shop__btn-wrap').css('top', `${imgHeight / 2}px`);
+
+  //$('.shop').append(parent);
+}
+
+function createFilters(data, parent) {
+  function createFilterMarkup(elem) {
+    return `
+          <li class="${elem.classes}" id="${elem.id}">
+              <button class="shop-btn filters__btn">
+                  <span class="filters__desc">${elem.title}</span>
+              </button>
+          </li>
+          `;
+  }
+
+  data.forEach((item) => {
+    parent.append(createFilterMarkup(item));
+  });
+}
